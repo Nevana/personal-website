2c0f66554446155fc8e8934a0fac95bd953687b6
Nevana <tim.harpe@t-online.de>
2019-04-14 13:21:04 +0200

    ssl encryption & new website components

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,10 +1,10 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()}
 function myFunction() {
-    var navbar = document.getElementById("myNavbar");
+    var navbar = document.getElementById("myNavbar")
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white"
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "")
     }
 }
 function toggleFunction() {
@@ -15,44 +15,14 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
-let counter = 0;
-function myProgress() {
-    let elem = document.getElementById("myProgress");   
-    if(counter == 0){
-        elem.style.width = 26 + '%';
-        document.getElementById("myLifeDesc").innerHTML = "(2005-2009) Elementary School </br>"
-        counter++;
-    }else if(counter == 1){
-        elem.style.width = 61 + '%';
-        document.getElementById("myLifeDesc").innerHTML += "(2009-2015) Middle School </br>"
-        counter++;
-    }else if(counter == 2){
-        elem.style.width = 83 + '%';
-        document.getElementById("myLifeDesc").innerHTML += "(2015-2018) Education as IT-Specialist @Deutsche Telelekom AG Darmstadt </br>"
-        counter++;
-    }else if(counter == 3){
-        elem.style.width = 99 + '%';
-        document.getElementById("myLifeDesc").innerHTML += "(2018-now) Employee as IT-Specialist & Developer @SST-IT Services AG Frankfurt </br>"
-        counter++;
-    }else if(counter == 4){
-        elem.style.width = 100 + '%';
-        document.getElementById("myLifeDesc").innerHTML += "(2018-now) Student to finish my highschool @Abendgymnasium Heppenheim </br>"
-        //document.getElementById("desire").innerHTML = "Reset"
-        counter++;
-    }else{
-        /*
-        elem.style.width = 0 + '%';
-        document.getElementById("myLifeDesc").innerHTML = "My life"
-        document.getElementById("desire").innerHTML = "Desire my life"
-        counter = 0;
-        */
-    }
-    
+function totopFunction() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
 }
 $("nav").find("a").click(function(e) {
-    e.preventDefault();
-    var section = $(this).attr("href");
+    e.preventDefault()
+    var section = $(this).attr("href")
     $("html, body").animate({
         scrollTop: $(section).offset().top
-    });
-});
+    })
+})
--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 const express = require('express')
-const app = new express()
-const port = 3000
+const https = require('https')
 const fs = require('fs')
+const app = express()
+const port = 443
 
 app.use('/assets', express.static(__dirname + '/assets'))
+
+let key = fs.readFileSync(__dirname + '/ssl/www_timharpe_me.key');
+let cert = fs.readFileSync(__dirname + '/ssl/www_timharpe_me.csr');
+let options = {
+    key: key,
+    cert: cert
+}
+
 
 //default Page
 app.get('/', function(req,res) {
     res.sendFile(__dirname + '/views/index.html')
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let server = https.createServer(options, app)
+server.listen(port, function(){
+    console.log(`server running at https://ip:${port}/`)
+});
+
+
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
