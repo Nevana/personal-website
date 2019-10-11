75b5befb7ef73550aa39bc6bbda7eb8f76ae1b80
Tim Harpe <tim.harpe@t-online.de>
2019-10-11 12:10:29 +0200

    change to http

--- src/server.js
+++ src/server.js
@@ -1,28 +1,16 @@
 const express = require('express')
-const https = require('https')
 const fs = require('fs')
 const app = express()
-const port = 443
+const port = 8080
 
 app.use('/assets', express.static(__dirname + '/assets'))
 
-//Set to your own keys!
-let key = fs.readFileSync(__dirname + '/ssl/www_timharpe_me.key');
-let cert = fs.readFileSync(__dirname + '/ssl/www_timharpe_me.csr');
-
-let options = {
-    key: key,
-    cert: cert
-}
-
-
 //default Page
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html')
 })
 
-//let server list on on $port
-let server = https.createServer(options, app)
-server.listen(port, function(){
+//start the server
+app.listen(port, function () {
     console.log(`server running at https://ip:${port}/`)
 });
